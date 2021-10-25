94e87d8d47abfc28a8bce9a85283865444c44aab
Varun <vkg0312@gmail.com>
2021-10-25 22:56:11 +0530

    Implemented callback in jupyter-notebook

--- ANN_implementation/src/training.py
+++ ANN_implementation/src/training.py
@@ -36,7 +36,7 @@ def training(config_path):
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
 
-    args.add_argument("--config", "-c", default="config.yaml")
+    args.add_argument("--config", "-c", default="../config.yaml")
 
     parsed_args = args.parse_args()
 
